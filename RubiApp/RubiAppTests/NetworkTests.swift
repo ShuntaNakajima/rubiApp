4d81d95b0b69efd6c473981c00aec2329b6bf6d3
shunta nakajima <shuntanakajima@shuntanoMacBook-Pro.local>
2019-08-17 19:01:53 +0900

    ライブラリとテストの追加

---
+++ RubiApp/RubiAppTests/NetworkTests.swift
@@ -0,0 +1,22 @@
+//
+//  NetworkTests.swift
+//  RubiApp
+//
+//  Created by shunta nakajima on 2019/08/17.
+//  Copyright © 2019 Shunta Nakajima. All rights reserved.
+//
+
+import XCTest
+@testable import RubiApp
+
+class NetworkTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
